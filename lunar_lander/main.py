c430e0581cff11451e6235f08713a288ed6d55f4
ahjerpe <hjerpe.adam@gmail.com>
2022-05-07 20:02:14 +0200

    visualize: add visualization of environment

--- lunar_lander/main.py
+++ lunar_lander/main.py
@@ -1,68 +1,31 @@
 import gym
-import numpy as np
-import matplotlib.pyplot as plt
 
 env = gym.make("LunarLander-v2")
-env.reset()
-
-plt.imshow(env.render('rgb_array'))
-print("Observation space:", env.observation_space)
-print("Action space:", env.action_space)
-
-print("taking action 2 (right)")
-new_obs, reward, is_done, _ = env.step(2)
-
-print("new observation code:", new_obs)
-print("reward:", reward)
-print("is game over?:", is_done)
-
-# Create env manually to set time limit. Please don't change this.
-TIME_LIMIT = 250
-env = gym.wrappers.TimeLimit(
-    gym.envs.classic_control.MountainCarEnv(),
-    max_episode_steps=TIME_LIMIT + 1,
-)
-actions = {'left': 0, 'stop': 1, 'right': 2}
+actions = {"nop": 0, "fire_left": 1, "main_engine": 2, "right_engine": 3}
 
 
 def policy(obs, t):
-    # Write the code for your policy here. You can use the observation
-    # (a tuple of position and velocity), the current time step, or both,
-    # if you want.
-    position, velocity = obs
-
-    # This is an example policy. You can try running it, but it will not work.
-    # Your goal is to fix that. You don't need anything sophisticated here,
-    # and you can hard-code any policy that seems to work.
-    # Hint: think how you would make a swing go farther and faster.
-    return actions['right']
+    return actions["nop"]
 
 
-plt.figure(figsize=(4, 3))
-from IPython import display
-display.clear_output(wait=True)
+def sample_policy(obs, t):
+    action = env.action_space.sample()
+    return action
 
-obs = env.reset()
-for t in range(TIME_LIMIT):
-    plt.gca().clear()
 
-    action = policy(obs, t)  # Call your policy
-    obs, reward, done, _ = env.step(
-        action)  # Pass the action chosen by the policy to the environment
-
-    # We don't do anything with reward here because MountainCar is a very simple environment,
-    # and reward is a constant -1. Therefore, your goal is to end the episode as quickly as possible.
-
-    # Draw game image on display.
-    plt.imshow(env.render('rgb_array'))
-
-    display.display(plt.gcf())
-    display.clear_output(wait=True)
-
-    if done:
-        print("Well done!")
-        break
-else:
-    print("Time limit exceeded. Try again.")
-
-display.clear_output(wait=True)
+TIME_LIMIT = 250
+EPISODE_LIMIT = 3
+for i_episode in range(EPISODE_LIMIT):
+    observation = env.reset()
+    for t in range(TIME_LIMIT):
+        env.render()
+        print(observation)
+        if i_episode % 2 == 0:
+            action = sample_policy(observation, t)
+        else:
+            action = policy(observation, t)
+        observation, reward, done, info = env.step(action)
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
