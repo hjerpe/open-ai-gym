a364643d5c7ac96a72e83711c9d1686ead58dc5e
Adam Hjerpe <hjerpe.adam@gmail.com>
2022-09-07 21:48:39 +0200

    Refactor add typing (#5)
    Add type hints

--- lunar_lander/main.py
+++ lunar_lander/main.py
@@ -1,12 +1,34 @@
 import gym
 import time
+import numpy as np
+import numpy.typing as npt
+
 
 env = gym.make("LunarLander-v2")
-actions = {"no_action": 0, "left_engine": 1, "main_engine": 2, "right_engine": 3}
+actions: dict[str, int] = {
+    "no_action": 0,
+    "left_engine": 1,
+    "main_engine": 2,
+    "right_engine": 3,
+}
 
 
-def heuristic_tyro(observation, t):
-    x_pos, y_pos, x_vel, y_vel, angle, angular_vel, leg1_contact, leg2_contact = observation
+def heuristic_tyro(observation: npt.NDArray[np.any], time_: int) -> int:
+    """
+    :param observation: An eight dimensional np.array representing a state.
+    :param time_: An integer denoting the current time step.
+    :return: An integer encoding an action.
+    """
+    (
+        x_pos,
+        y_pos,
+        x_vel,
+        y_vel,
+        angle,
+        angular_vel,
+        leg1_contact,
+        leg2_contact,
+    ) = observation
     obs_dict = {
         "x_pos": x_pos,
         "y_pos": y_pos,
@@ -15,33 +37,43 @@ def heuristic_tyro(observation, t):
         "angle": angle,
         "angular_vel": angular_vel,
         "leg1_contact": leg1_contact,
-        "leg2_contact": leg2_contact
+        "leg2_contact": leg2_contact,
     }
-    print(obs_dict)
-    action = None
-    if leg1_contact == 1 or leg2_contact == 1:
+    action: int = -1
+    if obs_dict["leg1_contact"] == 1 or obs_dict["leg2_contact"] == 1:
         action = actions["no_action"]
-    elif x_vel < -0.5:
+    elif obs_dict["x_vel"] < -0.5:
         action = actions["right_engine"]
-    elif x_vel > 0.5:
+    elif obs_dict["x_vel"] > 0.5:
         action = actions["left_engine"]
-    elif y_vel < -0.3:
+    elif obs_dict["y_vel"] < -0.3:
         action = actions["main_engine"]
-    elif angle < 0.05:
+    elif obs_dict["angle"] < 0.05:
         action = actions["left_engine"]
-    elif angle > 0.05:
+    elif obs_dict["angle"] > 0.05:
         action = actions["right_engine"]
-    elif y_vel < -0.1:
+    elif obs_dict["y_vel"] < -0.1:
         action = actions["main_engine"]
     else:
         action = actions["no_action"]
     return action
 
-def policy(obs, t):
+
+def zero_policy(observation: npt.NDArray[np.any], time_: int) -> int:
+    """
+    :param observation: An eight dimensional np.array representing a state.
+    :param time_: An integer denoting the current time step.
+    :return: An integer encoding no action.
+    """
     return actions["no_action"]
 
 
-def sample_policy(obs, t):
+def sample_policy(observation: npt.NDArray[np.any], time_: int) -> int:
+    """
+    :param observation: An eight dimensional np.array representing a state.
+    :param time_: An integer denoting the current time step.
+    :return: An integer encoding a random action.
+    """
     action = env.action_space.sample()
     return action
 
@@ -49,13 +81,13 @@ def sample_policy(obs, t):
 TIME_LIMIT = 300
 EPISODE_LIMIT = 5
 for i_episode in range(EPISODE_LIMIT):
-    observation = env.reset()
-    for t in range(TIME_LIMIT):
+    observation_, _ = env.reset()
+    for time_ in range(TIME_LIMIT):
         env.render()
         time.sleep(0.001)
-        action = heuristic_tyro(observation, t)
-        observation, reward, done, info = env.step(action)
-        if done:
-            print("Episode finished after {} timesteps".format(t+1))
+        action = heuristic_tyro(observation_, time_)
+        observation_, reward_, done_, info_ = env.step(action)
+        if done_:
+            print("Episode finished after {} timesteps".format(time_ + 1))
             break
 env.close()
