43dc13a289aa1d0575a16b074ff68299cf584e07
ahjerpe <hjerpe.adam@gmail.com>
2022-12-28 11:13:37 +0100

    Train NN model using policy gradients.

--- lunar_lander/main.py
+++ lunar_lander/main.py
@@ -2,12 +2,18 @@ import argparse
 import gym
 import time
 import numpy as np
+import math
 import numpy.typing as npt
 import mlflow
 import os
 import datetime
 import pickle
 import random
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Embedding, Reshape
+import tensorflow_probability as tfp
 
 
 env = gym.make("LunarLander-v2")
@@ -272,12 +278,160 @@ class QLearning:
         return action
 
 
+class PolicyModel:
+    def __init__(self, input_dim_: int, hidden_layers_: list[int], output_dim_: int):
+
+        self.model = Sequential()
+        self.model.add(Dense(32, input_dim=input_dim_, activation="relu"))
+        self.model.add(Dense(32, activation="relu"))
+        self.model.add(Dense(output_dim_, activation="softmax"))
+        print(self.model.summary())
+
+    def get_policy(self, obs):
+        probs = self.model(obs[np.newaxis])
+        return tfp.distributions.Categorical(probs=probs)
+
+    def action(self, obs):
+        smpl = self.get_policy(obs).sample()
+        return smpl
+
+    def save(self, name=None):
+        today_str = datetime.date.today().strftime("%Y%m%d")
+        if not name:
+            name = f"{self._get_model_dir()}/NN_{today_str}/"
+        folder_exists = os.path.exists(name)
+        if not folder_exists:
+            # Create a new directory because it does not exist
+            os.makedirs(name)
+        tf.keras.models.save_model(
+            self.model,
+            name,
+            overwrite=True,
+            include_optimizer=True,
+            save_format=None,
+            signatures=None,
+            options=None,
+            save_traces=True
+        )
+
+    def load(self, name: str = "./data/models/NN_20221228/"):
+        model_ = tf.keras.models.load_model(name)
+        model_.compile()
+        self.model = model_
+
+    def _get_model_dir(self):
+        return "./data/models/"
+
+
+def play_one_step(env, obs, pm: PolicyModel, loss_fn):
+    with tf.GradientTape() as tape:
+        action = pm.action(obs)
+        probs = pm.get_policy(obs).probs
+        y_target = tf.keras.utils.to_categorical(action, 4)
+        loss = tf.reduce_mean(loss_fn(y_target, probs))
+    grads = tape.gradient(loss, pm.model.trainable_variables)
+    obs, reward, done, info = env.step(int(action))
+    return obs, reward, done, grads
+
+
+def play_multiple_episodes(env, n_episodes, n_max_steps, model, loss_fn):
+    all_rewards = []
+    all_grads = []
+    for episode in range(n_episodes):
+        current_rewards = []
+        current_grads = []
+        obs = env.reset()
+        for step in range(n_max_steps):
+            obs, reward, done, grads = play_one_step(env, obs, model, loss_fn)
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+    return all_rewards, all_grads
+
+
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step] += discounted[step + 1] * discount_factor
+    return discounted
+
+
+def discount_and_normalize_rewards(all_rewards, discount_factor):
+    all_discounted_rewards = [
+        discount_rewards(rewards, discount_factor) for rewards in all_rewards
+    ]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [
+        (discounted_rewards - reward_mean) / reward_std
+        for discounted_rewards in all_discounted_rewards
+    ]
+
+
+def run_pg_training(n_iterations: int = 150):
+    input_dim = env.observation_space.shape[0]
+    output_dim = env.action_space.n
+    hidden_layers = [32]
+    pm = PolicyModel(input_dim, hidden_layers, output_dim)
+    n_episodes_per_update = 10
+    n_max_steps = 200
+    discount_factor = 0.95
+    learning_rate = 0.01
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    loss_fn = tf.keras.losses.CategoricalCrossentropy()
+
+    for iteration in range(n_iterations):
+        all_rewards, all_grads = play_multiple_episodes(
+            env=env,
+            n_episodes=n_episodes_per_update,
+            n_max_steps=n_max_steps,
+            model=pm,
+            loss_fn=loss_fn,
+        )
+        flat_rewards = np.concatenate(all_rewards)
+        print(f"At iteration: {iteration}")
+        print(f"Sum reward: {flat_rewards.sum()}")
+        print(f"Average reward: {flat_rewards.mean()}")
+        print(f"Std reward: {flat_rewards.std()}")
+        print(f"(min, max) reward: ({(flat_rewards.min(axis=0), flat_rewards.max())})")
+        all_final_rewards = discount_and_normalize_rewards(all_rewards, discount_factor)
+        all_mean_grads = []
+        for var_index in range(len(pm.model.trainable_variables)):
+            mean_grads = tf.reduce_mean(
+                [
+                    final_reward * all_grads[episode_index][step][var_index]
+                    for episode_index, final_rewards in enumerate(all_final_rewards)
+                    for step, final_reward in enumerate(final_rewards)
+                ],
+                axis=0,
+            )
+            all_mean_grads.append(mean_grads)
+        optimizer.apply_gradients(zip(all_mean_grads, pm.model.trainable_variables))
+
+    pm.save()
+
+
 def sample_policy() -> int:
     """
     :return: An integer encoding a random action.
     """
     action = env.action_space.sample()
     return int(action)
+
+
+run_pg_training(5000)
+
+
+
+input_dim = env.observation_space.shape[0]
+output_dim = env.action_space.n
+hidden_layers = [32]
+#pm = PolicyModel(input_dim, hidden_layers, output_dim)
+#pm.load()
 
 
 def main(args):
@@ -302,6 +456,9 @@ def main(args):
                 action = zero_policy()
             elif args.policy == "discretized-q-learning":
                 action = q_learn.next_action(state=observation_)
+            elif args.policy == "PG":
+                action = pm.action(obs=observation_)
+                action = tf.keras.backend.get_value(action)[0]
 
             observation_, reward_, done_, info_ = env.step(action)
             if done_:
@@ -323,28 +480,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Lunar Lander.")
     parser.add_argument(
         "--policy",
-        choices=["heuristic", "random", "zero", "discretized-q-learning"],
+        choices=["heuristic", "random", "zero", "discretized-q-learning", "PG"],
         default="discretized-q-learning",
         help="what policy to use",
     )
     parser.add_argument(
-        "--episodes",
-        type=int,
-        default=5,
-        help="how many episodes to run",
+        "--episodes", type=int, default=5, help="how many episodes to run",
     )
     parser.add_argument(
-        "--time-limit",
-        type=int,
-        default=300,
-        help="time limit for each episode",
+        "--time-limit", type=int, default=300, help="time limit for each episode",
     )
     parser.add_argument(
         "--mlflow-logging",
         action="store_true",
         help="""log experiment in MLFlow.
-        The following env variables needs to be set, MLFLOW_TRACKING_URI, MLFLOW_TRACKING_USERNAME, MLFLOW_TRACKING_PASSWORD
-        """,
+       The following env variables needs to be set, MLFLOW_TRACKING_URI, MLFLOW_TRACKING_USERNAME, MLFLOW_TRACKING_PASSWORD
+       """,
     )
     parser.add_argument(
         "--mlflow-experiment-name",
@@ -352,8 +503,7 @@ if __name__ == "__main__":
         help="the name of the MLflow experiment",
     )
     parser.add_argument(
-        "--mlflow-run-name",
-        help="the name of the MLflow run",
+        "--mlflow-run-name", help="the name of the MLflow run",
     )
 
     args = parser.parse_args()
